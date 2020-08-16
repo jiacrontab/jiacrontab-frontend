64cd53aac9386ac5fd5d2b28e0b267f3e59bf1eb
iwannay <772648576@qq.com>
2020-08-16 10:42:50 +0800

    备案信息

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,11 @@ import { Layout } from 'antd'
 
 class Footerjia extends React.Component {
     render() {
-        const currentYear = new Date().getFullYear();
+        const currentYear = new Date().getFullYear()
         return (
             <Layout.Footer style={{ textAlign: 'center', padding: 10 }}>
-                Jiacrontab ©{currentYear} Created by iwannay
+                Jiacrontab ©{currentYear} Created by iwannay{' '}
+                <a href="http://www.beian.miit.gov.cn">沪ICP备18002924号</a>
             </Layout.Footer>
         )
     }
